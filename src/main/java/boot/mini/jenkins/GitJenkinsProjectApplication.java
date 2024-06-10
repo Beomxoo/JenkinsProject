c9a742d8b37e9ce9612b88071363810576c09a18
Son <ssoo9271@naver.com>
2024-06-10 12:28:15 +0900

    0610

---
+++ src/main/java/boot/mini/jenkins/GitJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package boot.mini.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJenkinsProjectApplication.class, args);
+	}
+
+}
